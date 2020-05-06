7d7afc96fec84c6569f248be072de9f53d7a2640
FractalOfSolar <evosun9@gmail.com>
2020-05-06 21:38:16 +0500

    Вставил второе видео. Исправил стили для видео. Отрефаторил код, накладывающий постеры на видео.

--- js/body-end.js
+++ js/body-end.js
@@ -1,22 +1,28 @@
+const CLASS_VIDEO_WRAPPER = 'video_wrapper';
+const CLASS_TOP_BLOCK_VIDEO = 'top_block_video';
 const CLASS_CUSTOM_PLAY_BUTTON = 'custom_play_button';
 const CLASS_VIDEO_POSTER = 'video_poster';
 const CLASS_VIDEO_FRAME = 'video_frame';
 
 const VIDEOS = [
     {
-        className: 'top_block_video',
+        sectionClassName: 'main_info_section',
         poster: 'images/video/sport.jpg'
+    },
+    {
+        sectionClassName: 'dancing_section',
+        poster: 'images/video/dancing-group.jpg'
     }
 ];
 
 const videos = VIDEOS
     .map( video => ({
-        ...video, domElement: document.querySelector(`.${video.className}`)
+        ...video, domElement: document.querySelector(`.${video.sectionClassName} .${CLASS_VIDEO_WRAPPER}.${CLASS_TOP_BLOCK_VIDEO}`)
     }) )
     .filter( ({ domElement }) => domElement );
 
 
-for (const { className, poster, domElement } of videos) {
+for (const { poster, domElement } of videos) {
     domElement.addEventListener('click', () => {
         if (domElement.classList.contains(CLASS_CUSTOM_PLAY_BUTTON)) {
             domElement.classList.remove(CLASS_CUSTOM_PLAY_BUTTON);
